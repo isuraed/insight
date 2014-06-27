b74edde7f49919e42701d59c1365a8dec69def7d
Isura Edirisinghe <isurae@gmail.com>
2014-06-27 12:10:34 -0700

    Change script to get only reviews.

--- api/get_review.py
+++ api/get_review.py
@@ -3,8 +3,8 @@ import sys
 import urllib
 
 def main():
-    baseurl = 'http://localhost:5000/api/v0.1/'
-    requesturl = baseurl + sys.argv[1] + '/' + urllib.quote(sys.argv[2])
+    baseurl = 'http://localhost:5000/api/v0.1/reviews/'
+    requesturl = baseurl + urllib.quote(sys.argv[1])
     os.system("curl " + requesturl)
 
 
