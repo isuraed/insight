716ec3c1ed139217703c68ddc90ee886ed1cfbe4
Isura Edirisinghe <isurae@gmail.com>
2014-07-03 12:07:57 -0700

    Initial web interface

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import abort, Flask, jsonify
+from flask import abort, Flask, jsonify, render_template, request
 import happybase
 import json
 import re
@@ -75,6 +75,65 @@ def get_reviews_by_keyword(query):
         else:
             raise Exception("The product_id should be in the table!")
     return jsonify(results)
+
+
+@app.route('/products/<string:product_id>', methods = ['GET'])
+def get_product_reviews(product_id):
+    product_table = connection.table('isura_reviews_by_product_id')
+    reviews_row = product_table.row(product_id)
+    reviews = [] 
+    if reviews_row:
+        for r in reviews_row.itervalues():
+            reviews.append(json.loads(r))
+    return render_template('product.html', reviews=reviews)
+
+
+@app.route('/')
+def get_index():
+    query = request.args.get('q')
+    if not query:
+        return render_template('index.html')
+
+    product_table = connection.table('isura_reviews_by_product_id')
+    product_ids = get_product_ids(query)
+    results = []
+    for pid in product_ids:
+        row = product_table.row(pid)
+        if row:
+            reviews = row.values()
+            if reviews:
+                first = json.loads(reviews[0])
+                results.append(first)
+
+    return render_template('index.html', results=results)
+
+
+def get_product_ids(query):
+    query = query.lower()
+    keywords = re.split("[^a-z0-9]+", query)
+    title_table = connection.table('isura_products_by_title')
+    keyword_table = connection.table('isura_products_by_keyword')
+
+    # Try for exact match first.
+    title_row = title_table.row(query)
+    if title_row:
+        return title_row.values()
+        #reviews_sorted = sorted(reviews, key=lambda r: r['timestamp'])
+    else:
+        abort(404)
+
+    hit_count = {}
+    for w in keywords:
+        keyword_row = keyword_table.row(w)
+        if keyword_row:
+            for product_id in keyword_row.itervalues():
+                if product_id in hit_count:
+                    hit_count[product_id] += 1
+                else:
+                    hit_count[product_id] = 1
+    sorted_hits = sorted(hit_count.items(), key=lambda x:x[1])
+    sorted_ids = [i[0] for i in sorted_hits] 
+    return sorted_ids
 
 
 if __name__ == '__main__':
