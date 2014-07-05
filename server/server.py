800a4a8b5204cc2ade5e3aa7f18d6b7a1e48739e
Isura Edirisinghe <isurae@gmail.com>
2014-07-05 15:58:10 -0700

    Improve search by title functionality

--- server/server.py
+++ server/server.py
@@ -1,14 +1,34 @@
 #!flask/bin/python
-from flask import abort, Flask, jsonify, render_template, request
 import happybase
 import json
 import re
 import time
+from collections import OrderedDict
+from flask import abort, Flask, jsonify, render_template, request
+
+
+class Timer:
+    def __init__(self):
+        self.start_time = time.time()
+        self.stop_time = self.start_time
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.stop_time = time.time()
+
+    def elapsed_time(self):
+        return float('{:.3f}'.format(self.stop_time - self.start_time))
+
 
 API_VERSION = 'v0.3'
 
 app = Flask(__name__)
 connection = happybase.Connection('54.183.87.221')
+
+timer = Timer()
+
 
 @app.route('/api/' + API_VERSION + '/brand_metrics', methods = ['GET'])
 def get_brand_metrics():
@@ -32,8 +52,9 @@ def get_brand_metrics_for(brand):
     return jsonify( { 'brand_metrics' : row[0] } )
 
 
-@app.route('/api/' + API_VERSION + '/reviews/<string:product>', methods = ['GET'])
-def get_review_for(product):
+# v0.2 for backward compatibility for Kelty.
+@app.route('/api/v0.2/reviews/<string:product>')
+def get_reviews_for_old(product):
     start = time.time()
     table = connection.table('isura_reviews')
     # Keys are stored in lowercase in HBase for more flexible searching.
@@ -49,32 +70,40 @@ def get_review_for(product):
     return jsonify(reviews)
 
 
-@app.route('/api/' + API_VERSION + '/reviews/by-keyword/<string:query>', methods = ['GET'])
-def get_reviews_by_keyword(query):
-    title_table = connection.table('isura_products_by_title')
-    keyword_table = connection.table('isura_products_by_keyword')
-    product_table = connection.table('isura_reviews_by_product_id')
-    query = query.lower()
-    keywords = re.split("[^a-z0-9]+", query)
-    hits_count = {}
-    for w in keywords:
-        keyword_row = keyword_table.row(w)
-        if keyword_row:
-            for product_id in keyword_row.itervalues():
-                if product_id in hits:
-                    hits_count[product_id] += 1
-                else:
-                    hits_count[product_id] = 1
-    hits_sorted = sorted(hits_count.items(), key=lambda x:x[1])
-    hit_products = [h[0] for h in hits_sorted]
-    results = {}
-    for product_id in hits_products:
-        product_row = product_table.row(product_id)
-        if product_row:
-            results[product_id] = product_row.values()
-        else:
-            raise Exception("The product_id should be in the table!")
-    return jsonify(results)
+@app.route('/api/' + API_VERSION + '/reviews/id/<string:product_id>')
+def get_reviews_for_id(product_id):
+    timer.start()
+    table = connection.table('isura_reviews_by_product_id')
+    row = table.row(product_id)
+    reviews = [json.loads(r) for r in row.itervalues()]
+    reviews = sorted(reviews, key=lambda k: k['timestamp'])
+    timer.stop()
+    response = { 'reviews' : reviews }
+    response['meta'] = { 'count' : len(row), 'responseTime' : timer.elapsed_time() }
+    return jsonify(response)
+
+
+@app.route('/api/' + API_VERSION + '/reviews/search/<string:query>')
+def get_reviews_for(query):
+    if not query:
+        abort(404)
+
+    timer.start()
+    table = connection.table('isura_reviews_by_product_id')
+    product_ids = find_products(query)
+    all_reviews = []
+    review_count = 0
+    for pid in product_ids:
+        row = table.row(pid)
+        reviews = [json.loads(r) for r in row.itervalues()]
+        reviews = sorted(reviews, key=lambda k: k['timestamp'])
+        entry = { 'productId' : pid, 'count' : len(reviews), 'reviews' : reviews }
+        all_reviews.append(entry)
+        review_count += len(reviews)
+    timer.stop()
+    response = { 'reviews' : all_reviews }
+    response['meta'] = { 'productCount' : len(product_ids), 'reviewCount' : review_count, 'responseTime' : timer.elapsed_time() }
+    return jsonify(response)
 
 
 @app.route('/products/<string:product_id>', methods = ['GET'])
@@ -95,7 +124,7 @@ def get_index():
         return render_template('index.html')
 
     product_table = connection.table('isura_reviews_by_product_id')
-    product_ids = get_product_ids(query)
+    product_ids = find_products(query)
     results = []
     for pid in product_ids:
         row = product_table.row(pid)
@@ -108,32 +137,34 @@ def get_index():
     return render_template('index.html', results=results)
 
 
-def get_product_ids(query):
-    query = query.lower()
-    keywords = re.split("[^a-z0-9]+", query)
+def find_products(query):
     title_table = connection.table('isura_products_by_title')
     keyword_table = connection.table('isura_products_by_keyword')
+    result_limit = 25
 
-    # Try for exact match first.
-    title_row = title_table.row(query)
+    found_ids = []
+    title_row = title_table.row(query.lower())
     if title_row:
-        return title_row.values()
-        #reviews_sorted = sorted(reviews, key=lambda r: r['timestamp'])
-    else:
-        abort(404)
+        found_ids.extend(title_row.values())
 
+    if len(found_ids) >= result_limit:
+        return found_ids
+
+    keywords = re.split("[^a-z0-9']+", query.lower())
     hit_count = {}
-    for w in keywords:
-        keyword_row = keyword_table.row(w)
+    for kw in keywords:
+        keyword_row = keyword_table.row(kw)
         if keyword_row:
-            for product_id in keyword_row.itervalues():
-                if product_id in hit_count:
-                    hit_count[product_id] += 1
+            for pid in keyword_row.itervalues():
+                if pid in hit_count:
+                    hit_count[pid] += 1
                 else:
-                    hit_count[product_id] = 1
-    sorted_hits = sorted(hit_count.items(), key=lambda x:x[1])
-    sorted_ids = [i[0] for i in sorted_hits] 
-    return sorted_ids
+                    hit_count[pid] = 1
+
+    sorted_hits = sorted(hit_count.items(), key=lambda x:x[1], reverse=True)
+    found_ids.extend([i[0] for i in sorted_hits])
+    found_ids = list(OrderedDict.fromkeys(found_ids))
+    return found_ids[:min(len(found_ids), result_limit)]
 
 
 if __name__ == '__main__':
